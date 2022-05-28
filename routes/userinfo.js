d60170b28482ef26d1824267b466c016e6b9ad82
RanjanKiran707 <77231463+RanjanKiran707@users.noreply.github.com>
2022-05-28 17:12:49 +0530

    added get user info

--- routes/userinfo.js
+++ routes/userinfo.js
@@ -1,20 +1,32 @@
 const express = require('express');
-const userdetails= express.Router();
+const userdetails = express.Router();
 const User = require('../models/User');
 
 
-userdetails.post("/",(req,res)=>{
-    var bmi = (req.body.weight/(req.body.height*req.body.height))*10000;
+userdetails.post("/", (req, res) => {
+    var bmi = (req.body.weight / (req.body.height * req.body.height)) * 10000;
     bmi = Math.ceil(bmi)
-    var newValues = {$set:{bmi:bmi,daysSincePregnant:req.body.pregnantDays}};
-    const userinfo = User.updateOne({email:req.query.email},newValues,(err)=>{
-        if(err){
+    var newValues = { $set: { bmi: bmi, daysSincePregnant: req.body.pregnantDays } };
+    const userinfo = User.updateOne({ email: req.query.email }, newValues, (err) => {
+        if (err) {
             console.log(err)
         }
-        else{
+        else {
             res.send("User info is added to the database!")
         }
     })
 })
 
+
+// get details of user
+userdetails.get("/getInfo", (req, res) => {
+    const userinfo = User.findOne({ email: req.query.email }, (err, response) => {
+        if (err) {
+            res.status(300).send(err)
+        }
+        else {
+            res.send(response)
+        }
+    })
+})
 module.exports = userdetails
