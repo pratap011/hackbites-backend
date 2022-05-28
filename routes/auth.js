d78682f89b82cc3422b8ed242b68993cb3129b8f
RanjanKiran707 <77231463+RanjanKiran707@users.noreply.github.com>
2022-05-28 08:47:53 +0530

    Merge branch 'master' into ranjan

--- routes/auth.js
+++ routes/auth.js
@@ -3,53 +3,54 @@ const User = require('../models/User');
 
 
 
-auth.post('/register',async (req,res)=>{
+auth.post('/register', async (req, res) => {
     console.log("This is the register route!")
-    const userExists = User.findOne({email:req.body.email},(err,response)=>{
-        if(err){
-            console.log(err);}
-        else{
-            if(response){
-                res.send("The email already exists")
+    const userExists = User.findOne({ email: req.body.email }, (err, response) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (response) {
+                res.status(300).send("The email already exists")
             }
-            else{
+            else {
                 const user = new User({
-                    name:req.body.name,
-                    age:req.body.age,
-                    email:req.body.email,
-                    password:req.body.password
-               })
-               try{
+                    name: req.body.name,
+                    age: req.body.age,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                try {
                     const successUser = user.save();
-                    res.send("Success in adding the user!")  
-               }
-               catch(err){
-                   console.log(err)
-               }
+                    res.send("Success in adding the user!")
+                }
+                catch (err) {
+                    console.log(err)
+                }
             }
         }
     });
     console.log(userExists.email)
-  
+
 })
 
-auth.post("/login",(req,res)=>{
+auth.post("/login", (req, res) => {
     console.log("This is the login route");
-    const userLogin = User.findOne({email:req.body.email},(err,response)=>{
-        if(err){
+    const userLogin = User.findOne({ email: req.body.email }, (err, response) => {
+        if (err) {
             console.log(err)
         }
-        else{
-            if(!response){
-                res.send("There is no account with this email!");
+        else {
+            if (!response) {
+                res.status(300).send("There is no account with this email!");
 
             }
-            else{
-                if(req.body.password==response.password){
+            else {
+                if (req.body.password == response.password) {
                     res.send("You are successfully logged in.")
                 }
-                else{
-                    res.send("You have entered the wrong password.")
+                else {
+                    res.status(300).send("You have entered the wrong password.")
                 }
             }
         }
